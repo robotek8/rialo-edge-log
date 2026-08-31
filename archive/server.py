33ebe4c6eff6c84ee1abba06fbcdbba773b4d1a8
Alexandr Kutyrkin <83699597+robotek8@users.noreply.github.com>
2026-08-31 19:49:43 +0500

    feat: add trust signals and social previews

--- archive/server.py
+++ archive/server.py
@@ -41,6 +41,7 @@ STATIC_FILES = {
     "/github.svg": "github.svg",
     "/x.svg": "x.svg",
     "/telegram.svg": "telegram.svg",
+    "/og-image.png": "og-image.png",
     "/fonts/Oswald-Variable.ttf": "fonts/Oswald-Variable.ttf",
     "/favicon.svg": "favicon.svg",
     "/favicon.ico": "favicon.ico",
@@ -488,8 +489,13 @@ class ArchiveHandler(BaseHTTPRequestHandler):
             self.send_error(HTTPStatus.NOT_FOUND)
             return
         content_type = mimetypes.guess_type(filename)[0] or "application/octet-stream"
+        if content_type.startswith("text/") or content_type in {
+            "application/javascript",
+            "image/svg+xml",
+        }:
+            content_type += "; charset=utf-8"
         self.send_response(HTTPStatus.OK)
-        self.send_header("Content-Type", f"{content_type}; charset=utf-8")
+        self.send_header("Content-Type", content_type)
         self.send_header("Content-Length", str(len(payload)))
         self.send_header("Cache-Control", "no-cache")
         self.send_header(
